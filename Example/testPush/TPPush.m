cb475f5c1d3e758fc18618b7f81ef783c3b38824
唐鹏 <tangpeng@icarbonx.com>
2018-01-23 19:23:41 +0800

    init

---
+++ Example/testPush/TPPush.m
@@ -0,0 +1,25 @@
+//
+//  TPPush.m
+//  testPush
+//
+//  Created by tangpeng on 2018/1/23.
+//Copyright © 2018年 TPQuietBro. All rights reserved.
+//
+
+#import "TPPush.h"
+#import "JPUSHService.h"
+@implementation TPPush
+#pragma mark - init methods
+
+#pragma mark - system delegate
+
+#pragma mark - custom delegate
+
+#pragma mark - api methods
+
+#pragma mark - event response
+
+#pragma mark - private
+
+#pragma mark - getter / setter
+@end
---
+++ testPush/Classes/TPPush.h
@@ -0,0 +1,13 @@
+//
+//  TPPush.h
+//  testPush
+//
+//  Created by tangpeng on 2018/1/23.
+//Copyright © 2018年 TPQuietBro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TPPush : NSObject
+
+@end
